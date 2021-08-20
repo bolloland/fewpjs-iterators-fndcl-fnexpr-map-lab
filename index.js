039f85cfc2b1cf1f112fde511fa924f734ee11aa
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-08-20 01:44:35 -0500

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+    return tutorials.map(str => {
+      let split = str.split(" ")
+      let capitalized = split.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      let capitalizedArray = capitalized.join(" ")
+      console.log(capitalizedArray)
+      return capitalizedArray
+    })
 }
